00dae00df920583c4036c0ac8c88982df9b6619d
Sunny <loganayers2013@yahoo.com>
2019-06-27 18:06:55 -0500

    properly added the background

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     var winNumber = 0;
     var loseNumber = 0;
-    var userNumber = 0;
     var totalNumber = Math.floor(Math.random() * 120) + 19;
     //console.log(totalNumber);
     var userNumber = 0;
@@ -15,26 +14,42 @@ $(document).ready(function () {
     $(".winNumber").html("Wins: " + winNumber);
     $(".loseNumber").html("Losses: " + loseNumber);
     function initializeGame() {
+        totalNumber = Math.floor(Math.random() * 120) + 19;
+        //console.log(totalNumber);
+        userNumber = 0;
+        diamond = Math.floor(Math.random() * 12) + 1;
+        emerald = Math.floor(Math.random() * 12) + 1;
+        ruby = Math.floor(Math.random() * 12) + 1;
+        sapphire = Math.floor(Math.random() * 12) + 1;
 
+
+        $(".diamond").on("click", function () {
+            userNumber = userNumber + diamond;
+            $(".userNumber").html(userNumber);
+        });
+        $(".emerald").on("click", function () {
+            userNumber = userNumber + emerald;
+            $(".userNumber").html(userNumber);
+        });
+        $(".ruby").on("click", function () {
+            userNumber = userNumber + ruby;
+            $(".userNumber").html(userNumber);
+        });
+        $(".sapphire").on("click", function () {
+            userNumber = userNumber + sapphire;
+            $(".userNumber").html(userNumber);
+        });
+        if (userNumber === totalNumber) {
+            winNumber++;
+            $(".winNumber").html("Wins: " + winNumber);
+            initializeGame();
+        }
+
+        else if (userNumber > totalNumber) {
+            loseNumber++;
+            $(".loseNumber").html("losses: " + loseNumber);
+            initializeGame();
+        }
     }
-
-    $(".diamond").on("click", function () {
-        userNumber = userNumber + diamond;
-        $(".userNumber").html(userNumber);
-    });
-    $(".emerald").on("click", function () {
-        userNumber = userNumber + emerald;
-        $(".userNumber").html(userNumber);
-    });
-    $(".ruby").on("click", function () {
-        userNumber = userNumber + ruby;
-        $(".userNumber").html(userNumber);
-    });
-    $(".sapphire").on("click", function () {
-        userNumber = userNumber + sapphire;
-        $(".userNumber").html(userNumber);
-    });
-
-
     initializeGame();
 });
